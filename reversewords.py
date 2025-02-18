1d8282441ac0098301b48fe472fb8ba654a92efa
BlackHorse19 <arxgaming19@gmail.com>
2025-02-18 13:42:26 +0530

    reversewords.py

--- reversewords.py
+++ reversewords.py
@@ -1,5 +1,7 @@
-#In this problem, we are presented with a string s that consists of words separated by spaces. Our objective is to reverse the order of these words and return the resulting string with the words placed in opposite order from that of the input. A word is defined as a sequence of non-space characters, meaning that punctuation and letters are considered part of a word but spaces are not. Additionally, the given string s could have leading or trailing spaces and could also contain multiple spaces between words.
-
+#In this problem, we are presented with a string s that consists of words separated by spaces.
+#Our objective is to reverse the order of these words and return the resulting string with the words placed in opposite order from that of the input.
+#A word is defined as a sequence of non-space characters, meaning that punctuation and letters are considered part of a word but spaces are not. 
+#Additionally, the given string s could have leading or trailing spaces and could also contain multiple spaces between words.
 
 def reverseWords(s):
     
